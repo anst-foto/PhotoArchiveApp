332e84d8de0e2bdc066332e2367460f520562911
Starinin Andrey <starininandrey@gmail.com>
2024-05-26 09:47:37 +0300

    Добавление оповещения о ходе операции "релоцирования"

--- PhotoArchiveApp.CoreLib/FileService.cs
+++ PhotoArchiveApp.CoreLib/FileService.cs
@@ -11,36 +11,40 @@ public static class FileService
 
     public static IEnumerable<PhotoInfo> GetPhotosInfos(IEnumerable<string> files) => files.Select(PhotoInfoService.GetPhotoInfo);
 
-    public static void RelocatePhotos(IEnumerable<PhotoInfo> photos, string newPath)
+    public static bool RelocatePhotos(IEnumerable<PhotoInfo> photos, string newPath, IProgress<int>? progress = null)
     {
-        foreach (var photo in photos)
-        {
-            var oldPathFile = photo.Path.AbsolutePath;
-
-            var year = photo.DateTaken.Value.Year;
-            if (!Directory.Exists(@$"{newPath}\{year}"))
+            var count = 0;
+            foreach (var photo in photos)
             {
-                Directory.CreateDirectory(@$"{newPath}\{year}");
+                var oldPathFile = photo.Path.AbsolutePath;
+
+                var year = photo.DateTaken.Value.Year;
+                if (!Directory.Exists(@$"{newPath}\{year}"))
+                {
+                    Directory.CreateDirectory(@$"{newPath}\{year}");
+                }
+
+                var month = $"{photo.DateTaken.Value.Month:00}";
+                if (!Directory.Exists(@$"{newPath}\{year}\{year}-{month}"))
+                {
+                    Directory.CreateDirectory(@$"{newPath}\{year}\{year}-{month}");
+                }
+
+                var day = $"{photo.DateTaken.Value.Day:00}";
+                if (!Directory.Exists(@$"{newPath}\{year}\{year}-{month}\{year}-{month}-{day}"))
+                {
+                    Directory.CreateDirectory(@$"{newPath}\{year}\{year}-{month}\{year}-{month}-{day}");
+                }
+
+                var dateTime = photo.DateTaken.Value.ToString("yyyy-MM-dd_HH-mm-ss");
+                var ext = Path.GetExtension(oldPathFile);
+                var newPathFile = @$"{newPath}\{year}\{year}-{month}\{year}-{month}-{day}\{dateTime}{ext}";
+
+                File.Copy(oldPathFile, newPathFile);
+
+                progress?.Report(++count);
             }
-
-            var month = $"{photo.DateTaken.Value.Month:00}";
-            if (!Directory.Exists(@$"{newPath}\{year}\{year}-{month}"))
-            {
-                Directory.CreateDirectory(@$"{newPath}\{year}\{year}-{month}");
-            }
-
-            var day = $"{photo.DateTaken.Value.Day:00}";
-            if (!Directory.Exists(@$"{newPath}\{year}\{year}-{month}\{year}-{month}-{day}"))
-            {
-                Directory.CreateDirectory(@$"{newPath}\{year}\{year}-{month}\{year}-{month}-{day}");
-            }
-
-            var dateTime = photo.DateTaken.Value.ToString("yyyy-MM-dd_HH-mm-ss");
-            var ext = Path.GetExtension(oldPathFile);
-            var newPathFile = @$"{newPath}\{year}\{year}-{month}\{year}-{month}-{day}\{dateTime}{ext}";
-
-            File.Copy(oldPathFile, newPathFile);
-        }
+            return true;
     }
 
     private static bool IsPhoto(FileInfo file)
